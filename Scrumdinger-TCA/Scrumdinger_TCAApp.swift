d7881259267087c75467ebbf0c141ade620a025c
Bidisha Biswas <bdsh.14@gmail.com>
2023-11-03 20:49:39 -0600

    Initial Commit

---
+++ Scrumdinger-TCA/Scrumdinger_TCAApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scrumdinger_TCAApp.swift
+//  Scrumdinger-TCA
+//
+//  Created by Bidisha Biswas on 11/3/23.
+//
+
+import SwiftUI
+
+@main
+struct Scrumdinger_TCAApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
